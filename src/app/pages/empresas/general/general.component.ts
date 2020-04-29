ea243f23be12edbbb22c76d5ec01118fc2411445
karrziloo <karenlizbeth.dominguez@outlook.com>
2020-04-28 21:25:51 -0500

    get completo wii

--- src/app/pages/empresas/general/general.component.ts
+++ src/app/pages/empresas/general/general.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {EmpresasService} from '../../../shared/services/empresas/empresas.service';
 import {EmpresaModel} from '../../../shared/model/empresas/empresa.model';
+import { NgbActiveModal, NgbModal, ModalDismissReasons, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
+
 @Component({
   selector: 'app-general',
   templateUrl: './general.component.html',
@@ -9,8 +11,13 @@ import {EmpresaModel} from '../../../shared/model/empresas/empresa.model';
 export class GeneralComponent implements OnInit {
   public empresas = [];
   cargando = false;
- 
-  constructor(private empresaService: EmpresasService ) { }
+  closeResult: string;
+
+  constructor(private empresaService: EmpresasService, config: NgbModalConfig, private modalService: NgbModal ) {
+    // customize default values of modals used by this component tree
+    config.backdrop = 'static';
+    config.keyboard = false;
+  }
 
   ngOnInit(): void {
     this.cargando = true;
@@ -23,6 +30,28 @@ export class GeneralComponent implements OnInit {
     /*this.empresaService.post().subscribe( response =>
       console.log('Respuesta del servicio', response)
     );*/
+  }
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+
+  openVerticallyCentered(content) {
+    this.modalService.open(content, { centered: true });
   }
 
 }
