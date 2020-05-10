d19ac293e3e4a6f3272b2a93c6611fd9dfde18d3
Rafael Velez <rafael@yubbler.com>
2020-05-10 10:59:56 -0500

    Ejemplo headers para post wera

--- src/app/shared/services/empresas/empresas.service.ts
+++ src/app/shared/services/empresas/empresas.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { map, delay } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { Config } from '../../../config/index';
+import { RequestOptions } from '@angular/http';
 
 @Injectable({
   providedIn: 'root'
@@ -29,5 +30,20 @@ export class EmpresasService {
   }
   actualizarEmpresa(empresa, id) {
     return this.http.put(`${this.config.api}/business?id=${id}`, empresa);
+  }
+
+  crearEmpresaTest(empresa): Observable<any> {
+    let headers = new HttpHeaders({
+      'Content-Type': 'multipart/form-data;',
+      'name': empresa.name,
+      'contact': JSON.stringify(empresa.contact),
+      'email': empresa.email,
+      'phoneNumber': empresa.phoneNumber
+      //..Aqui se ponen los de mas, cuando el param es un objeto se usa json stringify
+     });
+    let options = { headers: headers };
+    
+    //Se meten los headrs al post
+    return this.http.post(`${this.config.api}/business`, empresa, options);
   }
 }
